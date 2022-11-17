104f8f58f2cfaad1b0b42e7588525a2a097ab98f
Niteshkumar <nkumar35101@gmail.com>
2022-11-17 22:35:56 +0530

    contract is tested and deployed

---
+++ hello.js
@@ -0,0 +1,10 @@
+const a = [1, 2, 3, 4, 5];
+const b = a;
+const [c] = a;
+const [d, e] = a;
+
+console.log("printing a", a);
+console.log("printing b", b);
+console.log("printing c", c);
+console.log("printing d", d);
+console.log("printing e", e);
